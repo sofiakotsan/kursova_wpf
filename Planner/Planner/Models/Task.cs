ff709132327a33a400b10eec923d5db6de10b303
sofiakotsan <sofiakotsan9@gmail.com>
2022-01-15 20:13:48 +0200

    added task class

--- Planner/Planner/Models/Task.cs
+++ Planner/Planner/Models/Task.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Planner.Models
 {
-    class Task
+    public enum TaskPriority
     {
+        Low,
+        Medium,
+        High
+    }
+    public class Task
+    {
+        private string text;
+        private DateTime date;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private TaskPriority priority;
+
+        public TaskPriority Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+
+        Task() : this ("empty", DateTime.Now, TaskPriority.Low) { }
+
+        public Task(string text, DateTime date, TaskPriority priority)
+        {
+            Text = text;
+            Date = date;
+            Priority = priority;
+        }
+
     }
 }
