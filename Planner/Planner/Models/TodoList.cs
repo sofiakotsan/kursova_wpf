7ad5f285326b19723eb8dce942c99448c3c9afe9
sofiakotsan <sofiakotsan9@gmail.com>
2022-01-15 20:21:44 +0200

    added todolist class

---
+++ Planner/Planner/Models/TodoList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Models
+{
+    class TodoList
+    {
+        private List<Task> tasks;
+
+        public List<Task> Tasks
+        {
+            get { return tasks; }
+            private set { tasks = value; }
+        }
+
+        public TodoList()
+        {
+            ReadTasks();
+        }
+
+        private void ReadTasks()
+        {
+            Tasks = new List<Task>()
+            {
+                new Task("task 1", DateTime.Now, TaskPriority.Low),
+                new Task("task 2", DateTime.Now, TaskPriority.Low),
+                new Task("task 3", DateTime.Now, TaskPriority.Low),
+                new Task("task 4", DateTime.Now, TaskPriority.Low),
+                new Task("task 5", DateTime.Now, TaskPriority.Low),
+                new Task("task 6", DateTime.Now, TaskPriority.Low),
+                new Task("task 7", DateTime.Now, TaskPriority.Low)
+            };
+        }
+
+    }
+}
