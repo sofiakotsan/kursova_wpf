9e1e5b84b3a8ef78b354de3a423a02e65be5cac6
sofiakotsan <sofiakotsan9@gmail.com>
2022-01-15 20:25:09 +0200

    added planner view and vm

--- Planner/Planner/Models/TodoList.cs
+++ Planner/Planner/Models/TodoList.cs
@@ -26,11 +26,11 @@ namespace Planner.Models
             Tasks = new List<Task>()
             {
                 new Task("task 1", DateTime.Now, TaskPriority.Low),
-                new Task("task 2", DateTime.Now, TaskPriority.Low),
-                new Task("task 3", DateTime.Now, TaskPriority.Low),
+                new Task("task 2", DateTime.Now, TaskPriority.High),
+                new Task("task 3", DateTime.Now, TaskPriority.Medium),
                 new Task("task 4", DateTime.Now, TaskPriority.Low),
-                new Task("task 5", DateTime.Now, TaskPriority.Low),
-                new Task("task 6", DateTime.Now, TaskPriority.Low),
+                new Task("task 5", DateTime.Now, TaskPriority.Medium),
+                new Task("task 6", DateTime.Now, TaskPriority.High),
                 new Task("task 7", DateTime.Now, TaskPriority.Low)
             };
         }
