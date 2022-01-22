565b944ea0ee2f0d00a84144093f5b21eda54674
sofiakotsan <sofiakotsan9@gmail.com>
2022-01-22 02:18:20 +0200

    added loading of tasks by specific date

--- Planner/Planner/ViewModels/PlannerVM.cs
+++ Planner/Planner/ViewModels/PlannerVM.cs
@@ -14,7 +14,7 @@ namespace Planner.ViewModels
 {
     class PlannerVM : VMBase
     {
-        private ObservableCollection<TaskVM> taskList;
+        // private ObservableCollection<TaskVM> taskList;
         private ObservableCollection<TaskVM> currentTaskList;
         private RelayCommand addCommand;
         private RelayCommand deleteCommand;
@@ -31,11 +31,20 @@ namespace Planner.ViewModels
         }
 
 
-        public ObservableCollection<TaskVM> TaskList
+        private IEnumerable<TaskVM> currentTasks;
+
+        public IEnumerable<TaskVM> CurrentTasks
+        {
+            get { return currentTasks; }
+            set { currentTasks = value; }
+        }
+
+
+        /*public ObservableCollection<TaskVM> TaskList
         {
             get { return taskList; }
             set { taskList = value; }
-        }
+        }*/
         public ObservableCollection<TaskVM> CurrentTaskList
         {
             get { return currentTaskList; }
@@ -81,7 +90,7 @@ namespace Planner.ViewModels
         }
         public PlannerVM()
         {
-            TaskList = new ObservableCollection<TaskVM>()
+            /*TaskList = new ObservableCollection<TaskVM>()
             {
                 new TaskVM(new Task("task 1")),
                 new TaskVM(new Task("task 2")),
@@ -89,13 +98,15 @@ namespace Planner.ViewModels
                 new TaskVM(new Task("task 4")),
                 new TaskVM(new Task("task 5")),
                 new TaskVM(new Task("task 6")),
-            };
+            };*/
 
             db = new ApplicationContext();
             db.Tasks.Load();
             Tasks = db.Tasks.Local.ToBindingList();
 
             CurrentTaskList = new ObservableCollection<TaskVM>();
+
+            CurrentTasks = db.Tasks.Where(task => task.Date == SelectedDate).ToList();
 
             SelectedDate = DateTime.Today;
             SetTasksByDate(SelectedDate);
@@ -107,22 +118,24 @@ namespace Planner.ViewModels
 
         private void AddTask(object obj)
         {
-            TaskVM newTask = new TaskVM(new Task("New Task"));
+            /*TaskVM newTask = new TaskVM(new Task("New Task"));
             TaskList.Add(newTask);
-            CurrentTaskList.Add(newTask);
+            CurrentTaskList.Add(newTask);*/
 
-            db.Tasks.Add(new TaskVM(new Task("New Task")));
+            db.Tasks.Add(new TaskVM(new Task("New Task", SelectedDate)));
             db.SaveChanges();
+            SetTasksByDate(SelectedDate);
         }
         private void DeleteTask(object obj)
         {
             if (MessageBox.Show("Are you sure you want to delete this task?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
             {
-                CurrentTaskList.Remove((TaskVM)obj);
-                TaskList.Remove((TaskVM)obj);
+                /*CurrentTaskList.Remove((TaskVM)obj);
+                TaskList.Remove((TaskVM)obj);*/
 
                 db.Tasks.Remove((TaskVM)obj);
                 db.SaveChanges();
+                SetTasksByDate(SelectedDate);
             }
         }
         private void CloseWindow(object obj)
@@ -132,14 +145,29 @@ namespace Planner.ViewModels
         private void SetTasksByDate(DateTime date)
         {
             CurrentTaskList.Clear();
+            //CurrentTaskList
+            //currentTaskList.
+            //currentTasks = from task in db.Tasks where task.Date == date select task;
+            //CurrentTasks = db.Tasks.Where(task => task.Date == date).ToList();
 
-            foreach (var task in TaskList)
+            foreach (var task in (db.Tasks.Where(task => task.Date == date)))
+            {
+                CurrentTaskList.Add(task);
+
+            }
+
+             //var val = CurrentTasks;
+
+            //CurrentTaskList = new ObservableCollection<TaskVM>(db.Tasks.Where(task => task.Date == date));
+            //CurrentTaskList.CollectionChanged();
+
+            /*foreach (var task in TaskList)
             {
                 if (task.Date == date)
                 {
                     CurrentTaskList.Add(task);
                 }
-            }
+            }*/
         }
 
 
